7638e03503f9202add4ff6ad2e1649f362c84a4d
faoziaziz <admin@labseni.com>
2020-08-26 01:36:12 +0700

    remove gitignore

---
+++ react-app/build/precache-manifest.1cac70f6d7c701e2349852a943a39318.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f169ab581d8ae8c350051eab4cc985a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82d59273c4fc17c135bc",
+    "url": "/static/css/main.e61e85f1.chunk.css"
+  },
+  {
+    "revision": "fd07c7645cbfc85e690c",
+    "url": "/static/js/2.e0765cd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e0765cd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82d59273c4fc17c135bc",
+    "url": "/static/js/main.0b46ca7a.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
