1cd28e73ddb47b2a85e0b306da6a2a177113c1eb
Aziz Faozi <admin@labseni.com>
2020-09-07 15:31:32 +0700

    mar

--- .#README.md
+++
@@ -1,1 +0,0 @@
-faoziaziz@faoziaziz-pc.662:1598600865
--- gfw-api/routes/api.js
+++ gfw-api/routes/api.js
@@ -12,20 +12,10 @@ router.get('/mantan', function(req, res, next) {
 
 /* Check for more */
 router.get("/apix", contApi.ok_reply);
-
-
 router.post("/testpost", contApi.test_post)
-
-
 router.post("/testbody", contApi.test_body)
-
-
 router.post("/testjson", contApi.test_json)
-
-
 router.post("/testmongo", contApi.test_mongo)
-
-
 router.get("/testmongo", contApi.test_mongo)
 router.get("/restjsontest", contApi.rest_json_test)
 
